d0bafdf703cdc918ce9d08261847c2019a470617
LK <laszlo.kovary@gmail.com>
2019-06-03 21:04:38 +0200

    added safe meta, FormsModule to home

--- src/app/layout/home/home.module.ts
+++ src/app/layout/home/home.module.ts
@@ -4,11 +4,13 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { MaterialModule } from 'src/app/material/material-module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
     MaterialModule,
     HomeRoutingModule
