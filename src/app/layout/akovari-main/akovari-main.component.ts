1e10d164e9f76c558ec46529dd274633237e8e16
LK <laszlo.kovary@gmail.com>
2020-08-11 23:09:55 +0200

    Akos FAI silver badge

--- src/app/layout/akovari-main/akovari-main.component.ts
+++ src/app/layout/akovari-main/akovari-main.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./akovari-main.component.scss']
 })
 export class AkovariMainComponent implements OnInit {
+  kaSilverWreathImagePath = 'assets/images/KovariAkosEzustkoszoru.png';
 
   constructor() { }
 
