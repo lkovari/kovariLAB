1bffee75c6f7f3d2c23a89996f9ae66f7dc348d1
LK <laszlo.kovary@gmail.com>
2020-08-11 21:03:19 +0200

    Update my sons graduates and publications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'KovariLAB';
-  lastUpdate = new Date('06/10/2019 11:13 PM');
+  lastUpdate = new Date('08/11/2020 08:17 PM');
 
   ngOnInit() {
     console.log('Last update ' + this.lastUpdate.toString());
