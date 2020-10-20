7f1ac9ea226764685a5c360dcf5654f21be31b05
LK <laszlo.kovary@gmail.com>
2020-10-20 16:49:19 +0200

    modify last update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'KovariLAB';
-  lastUpdate = new Date('10/19/2020 10:00 PM');
+  lastUpdate = new Date('10/20/2020 04:48 PM');
 
   /*
   @HostListener('contextmenu', ['$event'])
