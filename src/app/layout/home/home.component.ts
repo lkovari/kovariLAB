554d1eb1445c985924c2ef3c52e3748d658d0cde
LK <laszlo.kovary@gmail.com>
2020-10-20 16:53:32 +0200

    modified last update

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -7,7 +7,7 @@ import * as angular from '@angular/forms';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  lastUpdateDate = new Date('09/28/2020 11:18 AM');
+  lastUpdateDate = new Date('10/20/2020 04:52 PM');
   constructor() { }
 
   ngOnInit() {
