17735ee987f5533e021f1bb9b8ba35e54106d6ed
LK <laszlo.kovary@gmail.com>
2020-10-22 13:59:01 +0200

    Use MediaObserver and MediaChange to detect flex resized

--- src/app/layout/akovari-main/akovari-main.component.ts
+++ src/app/layout/akovari-main/akovari-main.component.ts
@@ -1,37 +1,77 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Subscription } from 'rxjs';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-akovari-main',
   templateUrl: './akovari-main.component.html',
   styleUrls: ['./akovari-main.component.scss']
 })
-export class AkovariMainComponent implements OnInit {
+export class AkovariMainComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('sidenav') sidenav: MatSidenav;
   pageOwner = 'Ákos Kővári';
   kaDiploma = 'assets/images/AkosGraduation2020.jpg';
   @ViewChild('videoPlayer') videoplayer: ElementRef;
   videoSource = 'assets/images/AkosTestAndTry20170719_164515.mp4';
   private SCREEN_WIDTH_LIMIT = 992;
+  mediaObserverAsObservable: Subscription;
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-      if (event.target.innerWidth < this.SCREEN_WIDTH_LIMIT) {
-        this.sidenav.close();
+  constructor(mediaObserver: MediaObserver) {
+    this.mediaObserverAsObservable = mediaObserver.asObservable().subscribe((changes: MediaChange[]) => {
+      /* option A.
+      const currentMediaChange = changes[0];
+      if (currentMediaChange.mqAlias === 'sm' || currentMediaChange.mqAlias === 'xs') {
+        if (this.sidenav) {
+          this.sidenav.close();
+        }
       } else {
-        this.sidenav.open();
+        if (this.sidenav) {
+          this.sidenav.open();
+        }
       }
+      */
+      // Option B.
+      const maxPriority = changes.reduce(
+        (currentMax, mediaChange) => (mediaChange.priority > currentMax ? mediaChange.priority : currentMax),
+        changes[0].priority
+      );
+      const currentMediaChange = changes.find(mc => mc.priority === maxPriority);
+      if (currentMediaChange) {
+        if (currentMediaChange.mqAlias === 'sm' || currentMediaChange.mqAlias === 'xs') {
+          if (this.sidenav) {
+            this.sidenav.close();
+          }
+        } else {
+          if (this.sidenav) {
+            this.sidenav.open();
+          }
+        }
+      }
+    });
   }
 
-  constructor() { }
+  private initializeSideNav() {
+    if (window.innerWidth < this.SCREEN_WIDTH_LIMIT) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    this.initializeSideNav();
+  }
+
   onMenuItemClick(event: MouseEvent) {
-    if (window.innerWidth < this.SCREEN_WIDTH_LIMIT) {
-      this.sidenav.close();
-    }
+    this.initializeSideNav();
     console.log('onMenuItemClick ' + event);
+  }
+
+  ngOnDestroy(): void {
+    this.mediaObserverAsObservable.unsubscribe();
   }
 }
--- src/app/layout/bkovari-main/bkovari-main.component.ts
+++ src/app/layout/bkovari-main/bkovari-main.component.ts
@@ -1,35 +1,75 @@
-import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bkovari-main',
   templateUrl: './bkovari-main.component.html',
   styleUrls: ['./bkovari-main.component.scss']
 })
-export class BkovariMainComponent implements OnInit {
+export class BkovariMainComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('sidenav') sidenav: MatSidenav;
   pageOwner = 'Bálint Kővári';
   private SCREEN_WIDTH_LIMIT = 992;
+  mediaObserverAsObservable: Subscription;
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-      if (event.target.innerWidth < this.SCREEN_WIDTH_LIMIT) {
-        this.sidenav.close();
+  constructor(mediaObserver: MediaObserver) {
+    this.mediaObserverAsObservable = mediaObserver.asObservable().subscribe((changes: MediaChange[]) => {
+      // option A.
+      const currentMediaChange = changes[0];
+      if (currentMediaChange.mqAlias === 'sm' || currentMediaChange.mqAlias === 'xs') {
+        if (this.sidenav) {
+          this.sidenav.close();
+        }
       } else {
-        this.sidenav.open();
+        if (this.sidenav) {
+          this.sidenav.open();
+        }
       }
+
+      /* Option B.
+      const maxPriority = changes.reduce(
+        (currentMax, mediaChange) => (mediaChange.priority > currentMax ? mediaChange.priority : currentMax),
+        changes[0].priority
+      );
+      const currentMediaChange = changes.find(mc => mc.priority === maxPriority);
+      if (currentMediaChange) {
+        if (currentMediaChange.mqAlias === 'sm' || currentMediaChange.mqAlias === 'xs') {
+          if (this.sidenav) {
+            this.sidenav.close();
+          }
+        } else {
+          if (this.sidenav) {
+            this.sidenav.open();
+          }
+        }
+      }
+      */
+    });
   }
 
-  constructor() { }
+  private initializeSideNav() {
+    if (window.innerWidth < this.SCREEN_WIDTH_LIMIT) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    this.initializeSideNav();
+  }
 
   onMenuItemClick(event: MouseEvent) {
-    if (window.innerWidth < this.SCREEN_WIDTH_LIMIT) {
-      this.sidenav.close();
-    }
+    this.initializeSideNav();
     console.log('onMenuItemClick ' + event);
+  }
+
+  ngOnDestroy(): void {
+    this.mediaObserverAsObservable.unsubscribe();
   }
 }
