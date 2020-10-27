7554ef7224b028058e388346727d4a26da8a7093
LK <laszlo.kovary@gmail.com>
2020-10-27 22:45:54 +0100

    adjust alignment

--- src/app/layout/akovari-main/akovari-certifications/akovari-cert.component.ts
+++ src/app/layout/akovari-main/akovari-certifications/akovari-cert.component.ts
@@ -9,10 +9,10 @@ export class AkovariCertComponent implements OnInit {
   kaSilverWreathImagePath = 'assets/images/KovariAkosEzustkoszoru.png';
   kaJuly102016Igctracklog = 'assets/images/AkosKovari_July102016_403Km_5h33mGlidingFlight.png';
   kankh = 'assets/images/AkosSPL.jpg';
+  kaicglog = 'assets/AkosKovari_July102016_2016-07-10-XCS-AAA-02.igc';
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
 }
