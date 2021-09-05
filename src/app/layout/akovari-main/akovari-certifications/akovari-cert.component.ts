ef8f822b9cf75b5f51e8112da346eaf0e84f20c7
LK <laszlo.kovary@gmail.com>
2021-09-05 21:05:05 +0200

    added grid to table

--- src/app/layout/akovari-main/akovari-certifications/akovari-cert.component.ts
+++ src/app/layout/akovari-main/akovari-certifications/akovari-cert.component.ts
@@ -15,6 +15,10 @@ export class AkovariCertComponent implements OnInit {
     { date: new Date('04/20/2021'), code: '031', name: 'Mass & Balance', occurrence: 1, percent: 92, result: ExamResult.PASSED },
     { date: new Date('04/20/2021'), code: '032', name: 'Performance (Aeroplane)', occurrence: 1, percent: 83, result: ExamResult.PASSED },
     { date: new Date('04/20/2021'), code: '081', name: 'Principles of Flight (Aeroplane)', occurrence: 1, percent: 90, result: ExamResult.PASSED },
+    { date: new Date('10/05/2021'), code: '', name: '', occurrence: 0, percent: 0, result: null },
+    { date: new Date('10/05/2021'), code: '', name: '', occurrence: 0, percent: 0, result: null },
+    { date: new Date('10/05/2021'), code: '', name: '', occurrence: 0, percent: 0, result: null },
+    { date: new Date('10/05/2021'), code: '', name: '', occurrence: 0, percent: 0, result: null }
   ];
 
   constructor() { }
